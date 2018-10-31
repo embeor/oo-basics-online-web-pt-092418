5fd621502845b0e27769f6509d5deab0ea2dc7c1
Emilio Betancourt <embeor@gmail.com>
2018-10-31 02:41:19 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,24 @@
-# Make your shoe class here!
+class Shoe
+
+  attr_accessor :color, :size, :material, :brand, :condition
+  attr_reader :brand
+
+  def initialize(brand)
+    @brand = brand
+  end
+
+  def condition=(condition)
+   @condition = "new"
+   @condition = "tattered"
+  end
+
+ def condition
+   @condition
+ end
+
+  def cobble
+    puts "Your shoe is as good as new!"
+    @condition = "tattered"
+  end
+
+end
